7aa7751786d590c8b5f7aa0e97b415a4df42d23f
Hillary Kaluuma <hkaluuma@idi.co.ug>
2024-07-05 10:14:44 +0300

    first commit

---
+++ Dockerize-PHP-Application/app/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hillary Docker project</title>
+</head>
+<body>
+     <p> Welcome Hillary to Docker for PHP </P>
+</body>
+</html>
